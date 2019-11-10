2ef56fea60da64e2e96eed961b642a04e8049cb5
SimonNeedham <simonneedham@github.com>
2019-11-10 00:06:24 +0000

    Create ConsoleApp project

---
+++ InstructorScanner.Console/Program.cs
@@ -0,0 +1,29 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace InstructorScanner.ConsoleApp
+{
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var loginUrl = @"https://wlacbooking.co.uk/Login.aspx";
+
+            var httpClient = new HttpClient();
+            var loginPageHtml = await httpClient.GetStringAsync(loginUrl);
+            var loginPageParser = new HtmlDocument();
+            loginPageParser.LoadHtml(loginPageHtml);
+
+            var inputs = loginPageParser.DocumentNode.SelectNodes("//form[@id='form1']//input");
+
+            foreach(var input in inputs)
+            {
+                Console.WriteLine($"{input.Id}: {input.GetAttributeValue("value", string.Empty)}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
