adea235b2d899f9851b154228a3c0a1d07b77e3f
SimonNeedham <simonneedham@github.com>
2019-11-18 00:46:38 +0000

    Wip

---
+++ InstructorScanner.FunctionApp/ScheduleInstructorScanStatusCheck.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using SendGrid.Helpers.Mail;
+using System.Threading.Tasks;
+
+namespace InstructorScanner.FunctionApp
+{
+    public class ScheduleInstructorScanStatusCheck
+    {
+        [FunctionName(nameof(ScheduleInstructorScanStatusCheck))]
+        public async Task Run(
+            [TimerTrigger("0 08 20 17 11 *")]TimerInfo myTimer,
+            ILogger logger,
+            [SendGrid(ApiKey = "AppSettings:SendGridApiKey")] IAsyncCollector<SendGridMessage> messageCollector
+        )
+        {
+        }
+    }
+}
