0fb38ea965fde11666b2076b14d20e6978867e65
SimonNeedham <simonneedham@github.com>
2019-11-21 12:03:19 +0000

    Removed old unsused project

--- InstructorScanner.Core/AvailabilityNames.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace InstructorScanner.Core
-{
-    public static class AvailabilityNames
-    {
-        public const string Free = "Free";
-        public const string Booked = "Booked";
-        public const string NA = "NA";
-    }
-}
--- InstructorScanner.Core/Slot.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace InstructorScanner.Core
-{
-    public class Slot
-    {
-        public string Time { get; set; }
-        public string Availability { get; set; }
-    }
-}
